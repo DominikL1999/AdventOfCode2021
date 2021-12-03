762408b3c283776b1ef7f7416c05b72a9b820d1d
Dominik <dominik.leko3@gmail.com>
2021-12-03 13:42:39 +0100

    Finish Day 3, Part 2

--- src/day3/main.cpp
+++ src/day3/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
 #include <cmath>
 #include <algorithm>
 #include <cassert>
@@ -10,6 +11,44 @@ using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+
+int convert_to_decimal(vector<bool>& bin_number) {
+    int n = 0;
+    for (int i = 0; i < bin_number.size(); i++) {
+        if (bin_number[i])
+            n += std::pow(2, bin_number.size() - i - 1);
+    }
+    return n;
+}
+
+int get_saturation(vector<vector<bool>>& diagnostic_report, bool diagnostic_criteria) {
+    int n_cols = diagnostic_report.front().size();
+    std::list<int> rows;
+    for (int i = 0; i < diagnostic_report.size(); i++) {
+        rows.push_front(i);
+    }
+
+    for (int col = 0; col < n_cols; col++) {
+        int count = std::count_if(rows.begin(), rows.end(), [col, diagnostic_report](auto row){return diagnostic_report[row][col] == 1;});
+
+        if (count * 2 >= rows.size()) {
+            rows.remove_if(
+                [col, diagnostic_report, diagnostic_criteria]
+                (auto row)
+                {return diagnostic_report[row][col] == (diagnostic_criteria != 1);});
+        }
+        else {
+            rows.remove_if(
+                [col, diagnostic_report, diagnostic_criteria]
+                (auto row)
+                {return diagnostic_report[row][col] == (diagnostic_criteria != 0);});
+        }
+
+        if (rows.size() == 1) break;
+    }
+
+    return convert_to_decimal(diagnostic_report[rows.front()]);
+}
 
 int main() {
     // input
@@ -27,24 +66,29 @@ int main() {
     assert(diagnostic_report.size() != 0);
     assert(diagnostic_report.front().size() != 0);
     
-    int rows = diagnostic_report.size();
-    int cols = diagnostic_report.front().size();
+    int n_cols = diagnostic_report.front().size();
 
     int gamma = 0;
 
     // Solution 1
-    for (int col = 0; col <= cols != 0; col++) {
+    for (int col = 0; col <= n_cols != 0; col++) {
         int count = std::count_if(diagnostic_report.begin(), diagnostic_report.end(), [col](auto row){return row[col] == 1;});
 
-        if (count > rows / 2)
-            gamma += std::pow(2, cols - col - 1);
+        if (count > diagnostic_report.size() / 2)
+            gamma += std::pow(2, n_cols - col - 1);
     }
 
-    int epsilon = std::pow(2, cols) - 1 - gamma;
+    int epsilon = std::pow(2, n_cols) - 1 - gamma;
     int solution = gamma * epsilon;
     cout << "Solution: " << solution << endl;
+    cout << "=================================" << endl;
 
     // Solution 2
+    int oxygen_generator_rating = get_saturation(diagnostic_report, false);
+    int co2_scrubber_rating = get_saturation(diagnostic_report, true);
 
-    
+    cout << "Oxygen Generator Rating: " << oxygen_generator_rating << endl;
+    cout << "CO2 Scrubber Rating: " << co2_scrubber_rating << endl;
+
+    cout << "Solution: " << oxygen_generator_rating * co2_scrubber_rating << endl;
 }
