9f1a212487f28f9c2f8f02705093a31fddc25be3
Dominik <dominik.leko3@gmail.com>
2021-12-04 13:27:36 +0100

    Finish Day 4, Part 1

--- src/day4/main.cpp
+++ src/day4/main.cpp
@@ -19,6 +19,66 @@ tuple<int, int, int> get_tuple(int index) {
     return {board, row, col};
 }
 
+bool check_win(int* boards, int board, int row, int col) {
+    // check row
+    bool winner_found = true;
+    for (int c = 0; c < 5; c++) {
+        if (boards[get_index(board, row, c)] != -1) {
+            winner_found = false;
+            break;
+        }
+    }
+    if (winner_found) return true;
+
+    // check col
+    winner_found = true;
+    for (int r = 0; r < 5; r++) {
+        if (boards[get_index(board, r, col)] != -1) {
+            winner_found = false;
+            break;
+        }
+    }
+    
+    return winner_found;
+}
+
+int get_sum(int* boards, int board) {
+    int sum = 0;
+    for (int r = 0; r < 5; r++) {
+        for (int c = 0; c < 5; c++) {
+            int index = get_index(board, r, c); 
+            if (boards[index] != -1)
+                sum += boards[index];
+        }
+    }
+    return sum;
+}
+
+int get_solution(vector<int>& numbers, int* boards, int number_of_boards) {
+    int solution = -1;
+    for (int i = 0; i < numbers.size(); i++) {
+        int n = numbers[i];
+        for (int b = 0; b < number_of_boards; b++) {
+            for (int r = 0; r < 5; r++) {
+                for (int c = 0; c < 5; c++) {
+                    if (boards[get_index(b, r, c)] == n) {
+                        boards[get_index(b, r, c)] = -1; // mark field as found
+
+                        // check for win
+                        if (check_win(boards, b, r, c)) {
+                            int sum = get_sum(boards, b);
+                            cout << "sum: " << sum << endl;
+                            cout << "n: " << n << endl;
+                            return sum * n;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    throw domain_error("No solution was found D:");
+}
+
 int main() {
     ifstream input("day4_input.txt");
 
@@ -59,6 +119,9 @@ int main() {
     }
 
     // Solution 1
+    int solution1 = get_solution(numbers, boards, number_of_boards);
+
+    cout << "Solution 1: " << solution1 << endl;
 
     delete boards;
 }
