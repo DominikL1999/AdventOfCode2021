c765e1ab9824ad8e5fb8e1fa03900c7b78a34650
Dominik <dominik.leko3@gmail.com>
2021-12-14 15:40:01 +0100

    Refactor Day 14

--- Day_9_to_24/AdventOfCode2021/Day14/Program.cs
+++ Day_9_to_24/AdventOfCode2021/Day14/Program.cs
@@ -36,134 +36,98 @@ namespace Day14
 
     internal class Program
     {
-        private const int STEPS = 10;
+        private const int STEPS1 = 10;
         private const int STEPS2 = 40;
 
-        // todo: change type to Dictionary<string, char>
-        private static Dictionary<string, string> insertionRules = new();
-
+        private static readonly Dictionary<string, char> insertionRules = new();
         private static readonly Dictionary<Problem, Dictionary<char, long>> solutions = new();
 
         private static void Main()
         {
-            // TEST
-            Dictionary<Problem, int> testDict = new();
-            Problem p1 = new("test", 10);
-            testDict.Add(p1, 10);
-            Console.WriteLine($"Test contains p1: {testDict.ContainsKey(p1)}");
-
             // Input
             var lines = File.ReadAllLines("input.txt");
             for (int i = 2; i < lines.Length; i++)
             {
                 var rule = lines[i].Split(" -> ");
-                insertionRules.Add(rule[0], rule[1]);
+                insertionRules.Add(rule[0], rule[1].Single());
             }
 
             // Solution 1
-            string polymer = lines[0];
-            for (int i = 0; i < STEPS; i++)
-            {
-                polymer = DoStep(polymer);
-            }
-            IEnumerable<IGrouping<char, char>> grouped = polymer.GroupBy(c => c);
-            foreach (var group in grouped)
-            {
-                Console.WriteLine($"{group.Key}, {group.Count()}");
-            }
-            int minCount = grouped.Min(group => group.Count());
-            int maxCount = grouped.Max(group => group.Count());
-
-            Console.WriteLine($"Difference in quantity of most common and least common element (10 steps): {maxCount - minCount}");
-            Console.WriteLine("===============================");
+            Dictionary<char, long> groups1 = GroupSizes(new(lines[0], STEPS1));
+            long solution1 = GetSolution(groups1);
+            Console.WriteLine($"Solution 2 ({STEPS1} steps): {solution1}");
 
             // Solution 2
-            Dictionary<char, long> groups = GroupSizes(new(lines[0], STEPS2));
-            foreach (var entry in groups)
-            {
-                Console.WriteLine($"{entry.Key} {entry.Value}");
-            }
-            long minCount2 = groups.Min(pair => pair.Value);
-            long maxCount2 = groups.Max(pair => pair.Value);
-            Console.WriteLine($"Difference: {maxCount2 - minCount2}");
+            Dictionary<char, long> groups2 = GroupSizes(new(lines[0], STEPS2));
+            long solution2 = GetSolution(groups2);
+            Console.WriteLine($"Solution 2 ({STEPS2} steps): {solution2}");
+        }
+
+        private static long GetSolution(Dictionary<char, long> groups)
+        {
+            long min = groups.Min(pair => pair.Value);
+            long max = groups.Max(pair => pair.Value);
+            return max - min;
         }
 
         private static Dictionary<char, long> GroupSizes(Problem problem)
         {
-            // todo: rename to solution
-            Dictionary<char, long> result;
-
-            if (solutions.TryGetValue(problem, out result)) return result;
+            Dictionary<char, long> solution = new();
 
             if (problem.StepsLeft == 0)
             {
-                result = CreateDictFromPolymer(problem.Polymer);
-                solutions.Add(problem, result);
-                return result;
+                solution = CreateDictFromPolymer(problem.Polymer);
+                solutions.Add(problem, solution);
+                return solution;
             }
 
-            result = new();
-
+            // Iterate over each pair of characters
             for (int i = 0; i < problem.Polymer.Length - 1; i++)
             {
                 string polymer = problem.Polymer.Substring(i, 2);
                 if (insertionRules.ContainsKey(polymer))
                 {
-                    string sub = insertionRules[polymer];
+                    char sub = insertionRules[polymer];
                     Problem problem1 = new(string.Concat(polymer[0], sub), problem.StepsLeft - 1);
                     Problem problem2 = new(string.Concat(sub, polymer[1]), problem.StepsLeft - 1);
-                    Dictionary<char, long> solution1;
-                    Dictionary<char, long> solution2;
                     // Solve for problem 1
-                    if (solutions.ContainsKey(problem1))
-                    {
-                        solution1 = solutions[problem1];
-                    }
-                    else
+                    if (!solutions.TryGetValue(problem1, out var solution1))
                     {
                         solution1 = GroupSizes(problem1);
                         solutions.TryAdd(problem1, solution1);
                     }
 
                     //Solve for problem 2
-                    if (solutions.ContainsKey(problem2))
-                    {
-                        solution2 = solutions[problem2];
-                    }
-                    else
+                    if (!solutions.TryGetValue(problem2, out var solution2))
                     {
                         solution2 = GroupSizes(problem2);
                         solutions.TryAdd(problem2, solution2);
                     }
 
                     // Join solutions for problem 1 and problem 2
-                    result = SumDicts(result, solution1);
-                    result = SumDicts(result, solution2);
-                    result[problem1.Polymer[0]]--;
-                    result[problem2.Polymer[0]]--;
+                    solution = SumDicts(solution, solution1);
+                    solution = SumDicts(solution, solution2);
+                    solution[problem1.Polymer[0]]--; // this was counted twice in this iteration
+                    solution[problem2.Polymer[0]]--; // this was counted twice in this iteration
                 }
                 else
-                {
-                    result = SumDicts(result, CreateDictFromPolymer(polymer));
+                { // This never happens if input is correct
+                    solution = SumDicts(solution, CreateDictFromPolymer(polymer));
                 }
             }
-            result[problem.Polymer[0]]++;
+            solution[problem.Polymer[0]]++; // this was counted one too few times
 
-            return result;
+            return solution;
         }
 
         private static Dictionary<char, long> SumDicts(Dictionary<char, long> dict1, Dictionary<char, long> dict2)
         {
-            Dictionary<char, long> result = new();
+            Dictionary<char, long> solution = new();
             foreach (var entry in dict1)
-            {
-                AddToDict(result, entry.Key, entry.Value);
-            }
+                AddToDict(solution, entry.Key, entry.Value);
             foreach (var entry in dict2)
-            {
-                AddToDict(result, entry.Key, entry.Value);
-            }
-            return result;
+                AddToDict(solution, entry.Key, entry.Value);
+            return solution;
         }
 
         private static void AddToDict(Dictionary<char, long> dict, char c, long x)
@@ -174,30 +138,10 @@ namespace Day14
 
         private static Dictionary<char, long> CreateDictFromPolymer(string polymer)
         {
-            Dictionary<char, long> result = new();
+            Dictionary<char, long> solution = new();
             foreach (var c in polymer)
-                AddToDict(result, c, 1);
-            return result;
-        }
-
-        private static string DoStep(string polymer)
-        {
-            List<Tuple<int, string>> insertions = new();
-            for (int i = 1; i < polymer.Length; i++)
-            {
-                if (insertionRules.TryGetValue(string.Concat(polymer[i - 1], polymer[i]), out string sub))
-                {
-                    insertions.Add(new(i, sub));
-                }
-            }
-
-            insertions.Reverse();
-            foreach (var insertion in insertions)
-            {
-                polymer = polymer.Insert(insertion.Item1, insertion.Item2);
-            }
-
-            return polymer;
+                AddToDict(solution, c, 1);
+            return solution;
         }
     }
 }
