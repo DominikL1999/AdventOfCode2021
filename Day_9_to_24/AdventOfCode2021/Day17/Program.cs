84107ae4ba8837abf0e58f5b09956344958878aa
Dominik <dominik.leko3@gmail.com>
2021-12-17 16:36:59 +0100

    Refactor Day 17

--- Day_9_to_24/AdventOfCode2021/Day17/Program.cs
+++ Day_9_to_24/AdventOfCode2021/Day17/Program.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using Utilities;
 
 namespace Day17
 {
     internal class Program
     {
-        //private const int xMin = 20;
-        //private const int xMax = 30;
-        //private const int yMin = -10;
-        //private const int yMax = -5;
-
         private const int xMin = 14;
         private const int xMax = 50;
         private const int yMin = -267;
@@ -19,14 +13,7 @@ namespace Day17
         private static void Main()
         {
             // Solution
-            int highestY = HighestY();
-            int count = CountSolutions();
-            Console.WriteLine($"highest y in any solution: {highestY}");
-            Console.WriteLine($"Number of feasible solutions: {count}");
-        }
-
-        private static int CountSolutions()
-        {
+            int maxHighestY = int.MinValue;
             int count = 0;
             for (int yVel = -yMin; yVel >= yMin; yVel--)
             {
@@ -34,32 +21,14 @@ namespace Day17
                 {
                     if (IsSolution(xVel, yVel, out int highestY))
                     {
+                        maxHighestY = Math.Max(maxHighestY, highestY);
                         count++;
                     }
                 }
             }
 
-            return count;
-        }
-
-        private static int HighestY()
-        {
-            int maxHighestY = int.MinValue;
-            for (int yVel = -yMin; yVel >= yMin; yVel--)
-            {
-                for (int xVel = 1; xVel <= xMax; xVel++)
-                {
-                    if (IsSolution(xVel, yVel, out int highestY))
-                    {
-                        //Console.WriteLine($"Solution: {xVel}, {yVel}");
-                        //Console.WriteLine($"Highest y in the solution: {highestY}");
-                        maxHighestY = Math.Max(maxHighestY, highestY);
-                        //return highestY;
-                    }
-                }
-            }
-
-            return maxHighestY;
+            Console.WriteLine($"highest y in any solution: {maxHighestY}");
+            Console.WriteLine($"Number of feasible solutions: {count}");
         }
 
         private static bool IsSolution(int xVel, int yVel, out int highestY)
@@ -81,38 +50,6 @@ namespace Day17
             }
 
             return false;
-        }
-
-        private static void DrawBoard()
-        {
-            for (int x = 0; x <= xMax; x++)
-            {
-                for (int y = 0; y >= yMin; y--)
-                {
-                    if (TargetHit(new(x, y)))
-                        Console.Write("T");
-                    else
-                        Console.Write(".");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static void DrawPath(HashSet<Position> positions)
-        {
-            for (int x = 0; x <= xMax; x++)
-            {
-                for (int y = 0; y >= yMin; y--)
-                {
-                    if (positions.Contains(new(x, y)))
-                        Console.Write("#");
-                    else if (TargetHit(new(x, y)))
-                        Console.Write("T");
-                    else
-                        Console.Write(".");
-                }
-                Console.WriteLine();
-            }
         }
 
         private static bool TargetHit(Position pos)
