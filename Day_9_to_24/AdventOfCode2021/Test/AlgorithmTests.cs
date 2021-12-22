5501497c883fef3a9f0c4092058361311f1e37b7
Dominik <dominik.leko3@gmail.com>
2021-12-22 09:31:58 +0100

    Add TryOperations and simple test for try operations.

---
+++ Day_9_to_24/AdventOfCode2021/Test/AlgorithmTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Utilities;
+using Xunit;
+
+namespace Test
+{
+    public class AlgorithmTests
+    {
+        [Fact]
+        public void TryOperationsTest1()
+        {
+            List<int> ints = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            bool changed = Algorithm.TryOperations(ints, new() { Remove20s });
+            Assert.False(changed);
+            Assert.Contains(10, ints);
+        }
+
+        [Fact]
+        public void TryOperationsTest2()
+        {
+            List<int> ints = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            bool changed = Algorithm.TryOperations(ints, new() { Remove10s });
+            Assert.True(changed);
+            Assert.DoesNotContain(10, ints);
+        }
+
+        [Fact]
+        public void TryOperationsTest3()
+        {
+            List<int> ints = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            bool changed = Algorithm.TryOperations(ints, new() { Remove20s, Remove10s });
+            Assert.True(changed);
+            Assert.DoesNotContain(10, ints);
+        }
+
+        private bool Remove10s(List<int> ints)
+        {
+            return ints.Remove(10);
+        }
+
+        private bool Remove20s(List<int> ints)
+        {
+            return ints.Remove(20);
+        }
+    }
+}
---
+++ Day_9_to_24/AdventOfCode2021/Utilities/Algorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utilities
+{
+    public static class Algorithm
+    {
+        /// <summary>
+        /// Tries all operations in order until the first succeeds.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Class type, given by reference to be modified.
+        /// </typeparam>
+        /// <param name="item">The parameter to modify.</param>
+        /// <param name="operations">
+        /// The operations that modify <paramref name="item" />. Return
+        /// true if the operation succeeded.
+        /// </param>
+        /// <returns><![CDATA[true]]> , if any operation succeeded</returns>
+        public static bool TryOperations<T>(T item, List<Func<T, bool>> operations) where T : class
+        {
+            foreach (var operation in operations)
+            {
+                if (operation(item))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
